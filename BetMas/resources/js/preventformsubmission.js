117dd0ee098e99683d66bac95fec1754ea7ac1d2
PietroLiuzzo <pietro.liuzzo@gmail.com>
2019-02-15 12:15:18 +0100

    closed https://github.com/BetaMasaheft/Documentation/issues/1023

--- BetMas/resources/js/preventformsubmission.js
+++ BetMas/resources/js/preventformsubmission.js
@@ -7,10 +7,10 @@ $('input').on('change paste keyup', function () {
         alert("You have used a reserved character, please avoid '.")
         $(this).parent().addClass('has-error')
         $('#submit-data').attr('disabled', 'disabled')
-        $('button[@type="submit"]').attr('disabled', 'disabled')
+        $('#f-btn-search').attr('disabled', 'disabled')
     } else {
         $(this).parent().removeClass('has-error')
         $('#submit-data').removeAttr('disabled')
-        $('button[@type="submit"]').removeAttr('disabled')
+        $('#f-btn-search').removeAttr('disabled')
     }
 });
