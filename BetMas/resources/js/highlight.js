0de79e59ed2b3cb8711a1db85f10f25c0fb02904
PietroLiuzzo <pietro.liuzzo@gmail.com>
2020-12-21 13:10:52 +0100

    Update highlight.js
    https://github.com/BetaMasaheft/Documentation/issues/1648

--- BetMas/resources/js/highlight.js
+++ BetMas/resources/js/highlight.js
@@ -1,12 +1,14 @@
 var getUrlParameter = function getUrlParameter(sParam) {
-    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
-    sURLVariables = sPageURL.split('&'),
+    var sPageURL = decodeURIComponent(window.location.href),
+    sPageParsIndex = sPageURL.lastIndexOf('\?'),
+     sPageParsed = sPageURL.substring(sPageParsIndex + 1),
+     sURLVariables = sPageParsed.split('&'),
     sParameterName,
     i;
-    
+    console.log(sURLVariables)
     for (i = 0; i < sURLVariables.length; i++) {
         sParameterName = sURLVariables[i].split('=');
-        
+    console.log(sParameterName)
         if (sParameterName[0] === sParam) {
             return sParameterName[1] === undefined ? true: sParameterName[1];
         }
