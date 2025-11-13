293b2c8b21b74d9aff8ce7c43c4198c274da0cba
duncdrum <d.paterson@me.com>
2025-11-13 13:59:21 +0100

    chore: Update Prettier check for XQuery files and improve formatting in Google module

--- db/apps/parser/modules/google.js
+++ db/apps/parser/modules/google.js
@@ -1,5 +1,7 @@
 window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+function gtag() {
+	dataLayer.push(arguments);
+}
+gtag("js", new Date());
 
-  gtag('config', 'UA-128203411-1');
+gtag("config", "UA-128203411-1");
